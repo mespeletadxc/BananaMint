235eb637e12d0821b4efbc493e1bebd13c275379
mespeletadxc <maria.espeleta@dxc.com>
2024-03-06 22:03:35 +0100

    entidades

--- src/main/java/com/banana/bananamint/services/IncomeExpenseService.java
+++ src/main/java/com/banana/bananamint/services/IncomeExpenseService.java
@@ -1,23 +1,40 @@
 package com.banana.bananamint.services;
 
-import com.banana.bananamint.payload.IncomeExpenseComparison;
 import com.banana.bananamint.domain.Income;
 import com.banana.bananamint.exception.IncomeExpenseException;
+import com.banana.bananamint.payload.IncomeExpenseComparison;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public interface IncomeExpenseService {
-    public List<Income> showAllIncomes(Long idCustomer) throws IncomeExpenseException;
+public class IncomeExpenseService implements IIncomeExpenseService{
+    @Override
+    public List<Income> showAllIncomes(Long idCustomer) throws IncomeExpenseException {
+        return null;
+    }
 
-    public Income addIncome(Long idCustomer, Income income) throws IncomeExpenseException;
+    @Override
+    public Income addIncome(Long idCustomer, Income income) throws IncomeExpenseException {
+        return null;
+    }
 
-    public List<Income> showAllExpenses(Long idCustomer) throws IncomeExpenseException;
+    @Override
+    public List<Income> showAllExpenses(Long idCustomer) throws IncomeExpenseException {
+        return null;
+    }
 
-    public Income addExpense(Long idCustomer, Income income) throws IncomeExpenseException;
+    @Override
+    public Income addExpense(Long idCustomer, Income income) throws IncomeExpenseException {
+        return null;
+    }
 
-    public List<Income> showAllExpensesByDateRange(Long idCustomer, LocalDate initDate, LocalDate finalDate) throws IncomeExpenseException;
+    @Override
+    public List<Income> showAllExpensesByDateRange(Long idCustomer, LocalDate initDate, LocalDate finalDate) throws IncomeExpenseException {
+        return null;
+    }
 
-    public List<IncomeExpenseComparison> getFinancialPerspective(Long idCustomer, LocalDate initDate, LocalDate finalDate) throws IncomeExpenseException;
-
+    @Override
+    public List<IncomeExpenseComparison> getFinancialPerspective(Long idCustomer, LocalDate initDate, LocalDate finalDate) throws IncomeExpenseException {
+        return null;
+    }
 }
