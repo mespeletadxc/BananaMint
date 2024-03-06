9badaf8ec99e6c42a0f14ff7a6180ec5a06741da
mespeletadxc <maria.espeleta@dxc.com>
2024-03-06 09:59:23 +0100

    primera carga

---
+++ src/main/java/com/microcompany/bananamint/bananamint/BananamintApplication.java
@@ -0,0 +1,13 @@
+package com.microcompany.bananamint.bananamint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananamintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananamintApplication.class, args);
+	}
+
+}
